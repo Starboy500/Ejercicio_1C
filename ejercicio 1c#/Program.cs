88a0e82a04ff006201e5b7b50eab0beb1ac1598b
Starboy500 <120419943+Starboy500@users.noreply.github.com>
2022-12-26 16:20:58 -0600

    Add files via upload

---
+++ ejercicio 1c#/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("cual es tu nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("hola, " + nombre);
+
+Console.WriteLine("introduce la hora");
+string hora = Console.ReadLine();
+
+Console.WriteLine("son las " + hora);
+
